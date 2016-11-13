d77fea74270ec2e5d73aa036bd023ca2f8dad5e9
Rafal Dittwald <rafal.dittwald@gmail.com>
2016-11-13 11:54:47 -0500

    Show a health-bar above each unit

--- src/clojure-warrior-web/styles.cljs
+++ src/clojure-warrior-web/styles.cljs
@@ -16,7 +16,24 @@
      {:width "64px"
       :height "64px"
       :background-repeat "no-repeat"
-      :background-position-x "center"}]]])
+      :background-position-x "center"}]
+
+    [:.health-bar
+     {:height "2px"
+      :background "#e5e6c7"
+      :margin "-60px auto 0"}
+
+     [:.health
+      {:height "100%"}
+
+      [:&.high
+       {:background "#11ec11"}]
+
+      [:&.medium
+       {:background "#e9ec11"}]
+
+      [:&.low
+       {:background "#ec1111"}]]]]])
 
 (defn styles-view []
   [:style
--- src/clojure-warrior-web/views/level.cljs
+++ src/clojure-warrior-web/views/level.cljs
@@ -2,18 +2,41 @@
 
 (def board
   [{:type :warrior
-    :state (rand-nth [:base :rest :attack :walk :shoot :dead]) }
+    :state (rand-nth [:base :rest :attack :walk :shoot :dead])
+    :max-health 20
+    :health (rand-int 20)}
    {:type :sludge
-    :state (rand-nth [:base :attack :dead])}
+    :state (rand-nth [:base :attack :dead])
+    :max-health 12
+    :health (rand-int 12)}
    {:type :thick-sludge
-    :state (rand-nth [:base :attack :dead])}
+    :state (rand-nth [:base :attack :dead])
+    :max-health 24
+    :health (rand-int 24)}
    {:type :archer
-    :state (rand-nth [:base :attack :dead])}
+    :state (rand-nth [:base :attack :dead])
+    :max-health 7
+    :health (rand-int 7)}
    {:type :wizard
-    :state (rand-nth [:base :shoot :dead])}
+    :state (rand-nth [:base :shoot :dead])
+    :max-health 3
+    :health (rand-int 3)}
    {:type :stairs}
    {:type :captive
-    :state (rand-nth [:base :free :dead])}])
+    :state (rand-nth [:base :free :dead])
+    :max-health 1
+    :health (rand-int 2)}])
+
+(defn health-bar-view [entity]
+  (when (entity :max-health)
+    [:div.health-bar
+     {:style {:width (* 2 (entity :max-health))}}
+     [:div.health
+      {:style {:width (* 2 (entity :health))}
+       :class (cond
+                (< (entity :health) 5) "low"
+                (< (entity :health) 10) "medium"
+                :else "high")}]]))
 
 (defn entity-view [entity]
   [:div.sprite
@@ -28,6 +51,7 @@
 
 (defn level-view []
   [:div.level
-   (for [space board]
+   (for [entity board]
      [:div.space
-      [entity-view space]])])
+      [entity-view entity]
+      [health-bar-view entity]])])
